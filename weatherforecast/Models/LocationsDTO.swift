291831707ea646532a6339eb4f32329d6f98c173
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-22 19:28:48 -0300

    chore: locations response dto

---
+++ weatherforecast/Models/LocationsDTO.swift
@@ -0,0 +1,14 @@
+//
+//  LocationsDTO.swift
+//  weatherforecast
+//
+//  Created by Gabriel Araújo Lima on 22/10/24.
+//
+
+import Foundation
+
+struct LocationsDTO {
+    let nome: String
+    let estado: String
+    let id: Int
+}
