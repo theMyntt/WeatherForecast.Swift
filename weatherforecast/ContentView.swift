0d9bfcc19cb561e306b897245d55a9e9fe1e9d77
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-22 19:26:14 -0300

    feat: initial commit

---
+++ weatherforecast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherforecast
+//
+//  Created by Gabriel Araújo Lima on 22/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherforecast/weatherforecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherforecastApp.swift
+//  weatherforecast
+//
+//  Created by Gabriel Araújo Lima on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherforecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
